19627be4241aa77870b9281c5edc5170d1fc12b9
Kwadwo <Kwadwon@gmail.com>
2014-12-22 20:30:38 -0500

    updated nav items

--- js/main.js
+++ js/main.js
@@ -24,20 +24,50 @@ $(document).ready(function(){
       nav.removeClass('navitem-below-text');
 			nav.removeClass('navitem-below', 500);
     }
+	
+		
+		if ((scrollPos > $("#main-pg-2").offset().top - $("#navbar").height() - 200) && 
+					(scrollPos < $(".projects").offset().top - $("#navbar").height() - 200)){
+			clearRedNow();
+			$("#mission").addClass('red-now');	
+		} else if ((scrollPos > $(".projects").offset().top - $("#navbar").height() - 200) && 
+					(scrollPos < $(".about").offset().top - $("#navbar").height() - 200)){
+			clearRedNow();
+			$("#projects").addClass('red-now');	
+		} else if (scrollPos > $(".about").offset().top - $("#navbar").height() - 200){
+			clearRedNow();
+			$("#about").addClass('red-now');	
+		} else {
+			clearRedNow();
+		}
   };
 
-  //scrolling
-  function doParallax(){
-		$('.page').each(function(){
-			var page = $(this);
-			var xcoord = page.css('backgroundPosition').split(' ')[0];
-			$(window).scroll(function(){
-				var yPos = -(($(window).scrollTop() - page.offset().top) / 10);
-				var coords = xcoord + ' ' + yPos+'px';
-				page.css({backgroundPosition:coords});
-				//page.animate({backgroundPositionX:xcoord,backgroundPositionY:yPos+'px'},100);
-			});
-		});
+	function clearRedNow(){
+		$("#mission").removeClass('red-now');
+		$("#projects").removeClass('red-now');
+		$("#about").removeClass('red-now');
 	}
+
+  //scrolling
+	$("#mission").click(function(event){
+		event.preventDefault();
+		$('body').animate({
+			scrollTop: $("#main-pg-2").offset().top - $("#navbar").height()
+		}, 1000);
+	});
+
+	$("#projects").click(function(event){
+		event.preventDefault();
+		$('body').animate({
+			scrollTop: $(".projects").offset().top - $("#navbar").height()
+		}, 1000);
+	});
+
+	$("#about").click(function(event){
+		event.preventDefault();
+		$('body').animate({
+			scrollTop: $(".about").offset().top - $("#navbar").height()
+		}, 1000);
+	});
 
 });
